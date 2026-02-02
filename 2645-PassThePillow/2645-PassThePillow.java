c43447e35ff7ee2dfca6c52679c5ca6c55c20833
SABARMATHI16 <sabarmathi456@gmail.com>
2026-02-02 14:28:04 +0530

    Create 2645-PassThePillow

---
+++ 2645-PassThePillow/2645-PassThePillow.java
@@ -0,0 +1,16 @@
+// Last updated: 2/2/2026, 2:28:16 PM
+
+ class Solution {
+    public int passThePillow(int n, int time) {
+        int completed = time/(n-1);
+        int remaining = time%(n-1);
+        int ans = 0;
+        if(completed%2!=0){
+            ans = n - remaining;
+        }else{
+            ans = remaining + 1;
+        }
+        return ans;
+    }
+}
+   
