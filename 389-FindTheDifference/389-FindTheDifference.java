acab5ea249a3d1e7318c9dafda1280015f887242
SABARMATHI16 <sabarmathi456@gmail.com>
2026-02-02 14:29:03 +0530

    Create 389-FindTheDifference

---
+++ 389-FindTheDifference/389-FindTheDifference.java
@@ -0,0 +1,11 @@
+// Last updated: 2/2/2026, 2:29:15 PM
+class Solution {
+    public char findTheDifference(String s, String t) {
+        char c = 0;
+        for (char x : s.toCharArray())
+            c ^= x;
+        for (char y : t.toCharArray())
+            c ^= y;
+        return c;
+    }
+}
